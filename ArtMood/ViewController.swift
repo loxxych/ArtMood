64175e6b38f55e63ea62f386eb715d6f2b8d120e
loxxych <kate.coconutt@gmail.com>
2026-03-15 13:32:59 +0300

    Initial Commit

---
+++ ArtMood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtMood
+//
+//  Created by loxxy on 15.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
